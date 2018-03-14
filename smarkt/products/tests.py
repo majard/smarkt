cd18f68c0f20b4b1c5fc167faf02b28fcffc64c2
majard <marlon_jardim_@hotmail.com>
2018-03-14 00:24:29 -0300

    Wrote the first test: creating a product. Added the urls, wrote the view and the product model to pass this first test.

--- smarkt/products/tests.py
+++ smarkt/products/tests.py
@@ -4,13 +4,17 @@ from django.test import TestCase
 
 from .models import Product
 
+PRODUCT_NAME = "Maçã"
+ANOTHER_PRODUCT_NAME = "Biscoito"
 
 class ProductViewTests(TestCase):
-	PRODUCT_NAME = "Maçã"
-	ANOTHER_PRODUCT_NAME = "Biscoito"
 
-    def test_product_creation(self):
-    	self.client.post('/products/', {'name': PRODUCT_NAME})
-    	product = Product.objects.get(id=1)
+	@classmethod
+	def setUpTestData(cls):
+        #Create a Product in the DB
+		Product.objects.create(name = PRODUCT_NAME)
 
-        self.assertEquals(product.name, PRODUCT_NAME)
+	def test_product_creation(self):
+		self.client.post('/products/', {'name': PRODUCT_NAME})
+		product = Product.objects.get(id=1)
+		self.assertEquals(product.name, PRODUCT_NAME)
