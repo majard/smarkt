be15368a2253b26777e8448131484ddf18d2f504
majard <marlon_jardim_@hotmail.com>
2018-03-15 13:58:26 -0300

    Set quantity field to 0 when saving a new product

--- smarkt/products/views.py
+++ smarkt/products/views.py
@@ -9,7 +9,7 @@ class CreateView(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         """Save the post data when creating a new product."""
-        serializer.save()
+        serializer.save(quantity=0)
 
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
     """This class handles the http GET, PUT and DELETE requests."""
