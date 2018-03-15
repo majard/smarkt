f115f2bd544e265d3a3ae496bc62ab6dc3e66671
majard <marlon_jardim_@hotmail.com>
2018-03-15 12:18:22 -0300

    refactored the tests for products code.

--- smarkt/products/tests.py
+++ smarkt/products/tests.py
@@ -27,44 +27,37 @@ class ProductViewTests(APITestCase):
 		Product.objects.create(name = PRODUCT_NAME)		
 		self.product = Product.objects.get()
 		self.serializer = ProductSerializer(self.product)
+		self.payload = {'name': ANOTHER_PRODUCT_NAME}
 
 	def test_api_can_create_a_product(self):
-		payload = {'name': ANOTHER_PRODUCT_NAME}
 		response = self.client.post(
 			reverse('create_product'), 
-			payload, format = "json"
-		)
+			self.payload, format = "json")
 		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 	def test_api_can_get_a_product(self):
 		"""Test the api can get a given product."""
 
-		#render a JSON for the expected product 
+		#render a JSON for the expected product response
 		expected_response = JSONRenderer().render(self.serializer.data)
 
 		response = self.client.get(
 		    reverse('product_details', kwargs = {'pk': self.product.id}),
-		    format="json"
-		)
+		    format="json")
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 		self.assertContains(response, expected_response)
 
 	def test_api_can_update_product(self):
 		"""Test the api can update a given product."""
-		change_product = {'name': ANOTHER_PRODUCT_NAME}
 		response = self.client.put(
 		    reverse('product_details', kwargs = {'pk': self.product.id}),
-		    change_product, format="json"
-		)
-
+		    self.payload, format="json")
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 	
 	def test_api_can_delete_product(self):
 		"""Test the api can delete a product."""
-		product = Product.objects.get()
 		response = self.client.delete(
-		    reverse('product_details', kwargs={'pk': product.id}),
+		    reverse('product_details', kwargs={'pk': self.product.id}),
 		    format='json',
-		    follow=True
-		)
+		    follow=True)
 		self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
