4472b10fff56449607bb660dbf0c91965c3e54fa
majard <marlon_jardim_@hotmail.com>
2018-03-15 14:04:07 -0300

    Added default=0 to model

--- smarkt/products/models.py
+++ smarkt/products/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    quantity = models.IntegerField(blank=True)
+    quantity = models.IntegerField(blank=True, default=0)
     average_price = models.DecimalField(max_digits=10, decimal_places=2,
      									null=True, blank=True)
