146b80be0875d0167e1da2ce787e7cd074386d9a
majard <marlon_jardim_@hotmail.com>
2018-03-15 12:33:31 -0300

    Fixed the quantity name for the receipt model.

--- smarkt/receipts/models.py
+++ smarkt/receipts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Receipt(models.Model):
     name = models.CharField(max_length=200)
-    quantitiy = models.IntegerField()
+    quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     average_price = models.DecimalField(max_digits=10, decimal_places=2,
      									null = True, blank = True)
