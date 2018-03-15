a959adf531feffc8dd807cd16d2e04c63c682d00
majard <marlon_jardim_@hotmail.com>
2018-03-15 13:04:40 -0300

    Fixed imports

--- smarkt/receipts/models.py
+++ smarkt/receipts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
 
 class Receipt(models.Model):
     name = models.CharField(max_length=200)
     quantity = models.IntegerField(validators = [MinValueValidator(1)])
-    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
+    price = models.DecimalField(max_digits=10, decimal_places=2, 
+    	validators=[MinValueValidator(Decimal('0.01'))])
     average_price = models.DecimalField(max_digits=10, decimal_places=2,
      									null = True, blank = True)
