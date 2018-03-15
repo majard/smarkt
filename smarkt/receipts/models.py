d6de26e21df92abd2946de40746bb3df43623ed2
majard <marlon_jardim_@hotmail.com>
2018-03-15 12:51:44 -0300

    Added validation to the receipt fields.

--- smarkt/receipts/models.py
+++ smarkt/receipts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Receipt(models.Model):
     name = models.CharField(max_length=200)
-    quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField(validators = [MinValueValidator(1)])
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))])
     average_price = models.DecimalField(max_digits=10, decimal_places=2,
      									null = True, blank = True)
