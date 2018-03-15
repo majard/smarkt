52ed78713771d86c3c78b173fe6101a62b17cc16
majard <marlon_jardim_@hotmail.com>
2018-03-15 12:23:14 -0300

    Wrote receipt model and made migrations to the database.

--- smarkt/receipts/models.py
+++ smarkt/receipts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Receipt(models.Model):
+    name = models.CharField(max_length=200)
+    quantitiy = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    average_price = models.DecimalField(max_digits=10, decimal_places=2,
+     									null = True, blank = True)
